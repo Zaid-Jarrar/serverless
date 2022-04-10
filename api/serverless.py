9d5d07da256be0b080f24c99d1a3e307e03ce699
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-04-10 17:51:31 +0300

    fix

--- api/serverless.py
+++ api/serverless.py
@@ -7,5 +7,9 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
+    question = input("Enter a number: ")
+    self.wfile.write(bytes(question, "utf-8"))
+    multi = int(question) ** 2
+    self.wfile.write(bytes(str(multi), "utf-8"))
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
