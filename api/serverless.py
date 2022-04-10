156339995426b5e21e76588b17b4eac1ce7644cb
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-04-10 17:44:37 +0300

    fix

--- api/serverless.py
+++ api/serverless.py
@@ -7,6 +7,5 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now('GMT+3').strftime('%Y-%m-%d %H:%M:%S')).encode())
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
-    
