eb8682855b9d939bf41f4a77fa2c80f1a188ca22
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-04-10 17:26:43 +0300

    adding new feature

--- api/sqr.py
+++ api/sqr.py
@@ -1,3 +1,5 @@
+import math
+
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 
@@ -7,6 +9,9 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now('GMT+3').strftime('%Y-%m-%d %H:%M:%S')).encode())
+    sqr = math.sqrt(int(self.path[1:]))
+    self.wfile.write(str(sqr).encode())
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
     
+
