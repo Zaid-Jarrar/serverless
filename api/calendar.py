a290d04049fab61d3b441779efeb068683f1ac57
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-04-10 21:22:45 +0300

    test

--- api/calendar.py
+++ api/calendar.py
@@ -8,9 +8,11 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.path = 'large_m12Yz5uRK2DL5UCAZMbeB3ArNNg.jpg'
-    with open(self.path, 'rb') as f:
-      self.wfile.write(f.read())
+    # self.path = 'large_m12Yz5uRK2DL5UCAZMbeB3ArNNg.jpg'
+    # with open(self.path, 'rb') as f:
+    #   self.wfile.write(f.read())
     url= urlparse('https://serverless-eight-tawny.vercel.app/api/serverless')
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d-%W %H:%M:%S')).encode())
     url.path
     return
+
