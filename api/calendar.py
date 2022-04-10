71818c1412395aa6e94b07f4ff443e41cd4f2ec7
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-04-10 21:36:12 +0300

    test

--- api/calendar.py
+++ api/calendar.py
@@ -14,10 +14,10 @@ class handler(BaseHTTPRequestHandler):
     #   self.wfile.write(f.read())
     url= urlparse('https://serverless-eight-tawny.vercel.app/api/serverless')
     message1 = 'This is the datetime Year/Month/Day/Number of Week/Hour/Minute/Second: ' +'\n'
-    message2 = 'This is the Calendar for year 2022: ' +'\n'
+    message2 = 'This is the Calendar for year 2022: ' +'\n\n'
 
     self.wfile.write(message1.encode())
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d/%W/ %H:%M:%S')).encode())
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d/%W/ %H:%M:%S \n')).encode())
     self.wfile.write(message2.encode())
     self.wfile.write(calendar.calendar(2022, 2, 1, 6).encode())
     url.path
