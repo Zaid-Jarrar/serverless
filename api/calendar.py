52b6d0385e73882f5576a86f6555376ac3c5c0bc
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-04-10 22:01:21 +0300

    test

--- api/calendar.py
+++ api/calendar.py
@@ -15,7 +15,8 @@ class handler(BaseHTTPRequestHandler):
     #   self.wfile.write(f.read())
     url= 'https://serverless-eight-tawny.vercel.app/api/calendar'
     result = urllib.parse.urlparse(url)
-    self.wfile.write(result.encode('utf-8'))
+    result.query
+    result.path
     # self.wfile.write('Scheme : ' + result.scheme)
     # self.wfile.write('HostName : ' + result.hostname)
     # self.wfile.write('Query : ' + result.query)
