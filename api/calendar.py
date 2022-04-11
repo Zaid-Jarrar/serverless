4b338dca820126d21535463df04a6af8048331fc
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-04-11 14:08:53 +0300

    test2

--- api/calendar.py
+++ api/calendar.py
@@ -1,37 +1,36 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+from urllib import parse
+import platform
 import calendar
-# from urllib.parse import urlparse
-import urllib.parse
 
 class handler(BaseHTTPRequestHandler):
 
-  def do_GET(self):
+  def do_GET(self):    
+    
+    path= self.path
+    url_components = parse.urlparse(path)
+    query_string = parse.parse_qsl(url_components.query)
+    dic=dict(query_string)
+    name= dic.get('name')
+    if name:
+      message = f'Hello, {name}!\n'
+    else:
+      message = 'Hello, Stranger!\n' 
+
+    message += f"\n Greetings from Python Version {platform.python_version()} at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n"
+    message1 = '\nThis is the datetime Year/Month/Day/Number of Week/Hour/Minute/Second: ' +'\n'
+    message2 = '\nThis is the Calendar for year 2022: ' +'\n\n'
+
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    # self.path = 'large_m12Yz5uRK2DL5UCAZMbeB3ArNNg.jpg'
-    # with open(self.path, 'rb') as f:
-    #   self.wfile.write(f.read())
-    url= 'https://serverless-eight-tawny.vercel.app/api/calendar'
-    result = urllib.parse.urlparse(url)
-    result.query
-    result.path
-    # self.wfile.write('Scheme : ' + result.scheme)
-    # self.wfile.write('HostName : ' + result.hostname)
-    # self.wfile.write('Query : ' + result.query)
-
-
-    
-
-    message1 = 'This is the datetime Year/Month/Day/Number of Week/Hour/Minute/Second: ' +'\n'
-    message2 = 'This is the Calendar for year 2022: ' +'\n\n'
-
+    self.wfile.write(message.encode())
     self.wfile.write(message1.encode())
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d/%W/ %H:%M:%S \n')).encode())
     self.wfile.write(message2.encode())
     self.wfile.write(calendar.calendar(2022, 2, 1, 6).encode())
-    url.params
-    self.wfile.write(url.query.encode())
+
     return
+
 
